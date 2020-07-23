ab1dc9816b56c9e19b98e19eb44c0f5b4b155e34
jneilliii <jneilliii+github@gmail.com>
2020-07-23 10:47:10 -0400

    update sponsors

--- octoprint_temp_control_tab/__init__.py
+++ octoprint_temp_control_tab/__init__.py
@@ -3,9 +3,8 @@ from __future__ import absolute_import, division, print_function
 
 import octoprint.plugin
 
-class TempControlTabPlugin(octoprint.plugin.TemplatePlugin,
-                           octoprint.plugin.AssetPlugin):
-						   
+class TempControlTabPlugin(octoprint.plugin.TemplatePlugin, octoprint.plugin.AssetPlugin):
+
 	def get_template_configs(self):
 		return [
 			dict(type="tab", name="Temp & Control", custom_bindings=False)
@@ -19,28 +18,24 @@ class TempControlTabPlugin(octoprint.plugin.TemplatePlugin,
 			temp_control_tab=dict(
 				displayName="Temp Control Tab",
 				displayVersion=self._plugin_version,
-
-				# version check: github repository
 				type="github_release",
 				user="jneilliii",
 				repo="OctoPrint-TempControlTab",
 				current=self._plugin_version,
-
-				# update method: pip
 				pip="https://github.com/jneilliii/OctoPrint-TempControlTab/archive/{target_version}.zip"
 			)
 		)
-		
+
 __plugin_name__ = "Temp Control Tab"
 
 def __plugin_load__():
 	global __plugin_implementation__
 	__plugin_implementation__ = TempControlTabPlugin()
-	
+
 	global __plugin_hooks__
 	__plugin_hooks__ = {
 		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.update_hook
 		}
-	
-	global __plugin_settings_overlay__
-	__plugin_settings_overlay__ = dict(appearance=dict(components=dict(order=dict(tab=["temp","plugin_temp_control_tab"]))))
+
+	# global __plugin_settings_overlay__
+	# __plugin_settings_overlay__ = dict(appearance=dict(components=dict(order=dict(tab=["temp","plugin_temp_control_tab"]))))
